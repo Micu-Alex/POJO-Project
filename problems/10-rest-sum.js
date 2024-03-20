ee1f0515606a56de6ca79a4b9105985cdc85711c
Micu-Alex <alexmicu389@gmail.com>
2024-03-20 03:19:33 +0200

    solved 10

--- problems/10-rest-sum.js
+++ problems/10-rest-sum.js
@@ -11,7 +11,12 @@ restSum(0); // => 0
 
 function restSum(...otherNums) {
   // Your code here
+  return otherNums.reduce((accum, curentValue) =>
+     accum + curentValue
+    )
 }
-
+console.log(
+  restSum(1, 2, 3, 4, 5, 6, 7, 8, 9) // => 45
+);
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = restSum;
