b0efa6a9a7e34584bc7e2783becc49ebdc3b57f4
Micu-Alex <alexmicu389@gmail.com>
2024-03-20 02:38:02 +0200

    solved 06

--- problems/06-key-in-object-array.js
+++ problems/06-key-in-object-array.js
@@ -21,7 +21,27 @@ keyInObjectArray(objArray, 'animal'); // => false
 
 function keyInObjectArray(objArray, keyString) {
   // Your code here
+  let hasKey = false
+  objArray.forEach(obj => {
+    if (obj.hasOwnProperty(keyString)) {
+      hasKey = true;
+    }
+  });
+  return hasKey;
 }
 
+let objArray = [
+  { name: "Rupert" },
+  { age: 42 },
+  { planet: "Earth", system: "Milky Way" }
+];
+
+console.log(
+
+  keyInObjectArray(objArray, 'planet'), // => true
+  keyInObjectArray(objArray, 'age'), // => true
+  keyInObjectArray(objArray, 'food'),// => false
+  keyInObjectArray(objArray, 'animal') // => false
+);
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = keyInObjectArray;
